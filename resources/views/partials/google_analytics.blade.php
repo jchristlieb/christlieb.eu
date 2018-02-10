d12f0816b4e10fdefcdebb8c966fc40552315575
Manuel Christlieb <manuel@christlieb.eu>
2018-02-10 13:27:00 +0100

    add logo

--- resources/views/partials/google_analytics.blade.php
+++ resources/views/partials/google_analytics.blade.php
@@ -6,6 +6,7 @@
         ga.q = [];
         ga.l = +new Date;
         ga('create', '{{config('blog.google_analytics')}}', 'auto');
+        ga('set', 'anonymizeIp', true);
         ga('send', 'pageview')
     </script>
     <script src="https://www.google-analytics.com/analytics.js" async defer></script>
