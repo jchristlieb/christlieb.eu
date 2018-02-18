792eacadd26470ef634802d5f7cbd0b5db4696ce
Manuel Christlieb <manuel@christlieb.eu>
2018-02-18 04:39:42 +0100

    remove example component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -61,7 +61,6 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
 Vue.component('tags-widget', require('./components/TagsWidget.vue'));
 Vue.component('update-tag', require('./components/UpdateTagComponent.vue'));
 
