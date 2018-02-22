bbbaba882a60026784b5cb14b591e34f8afb7665
Manuel Christlieb <manuel@christlieb.eu>
2018-02-22 20:28:52 +0100

    add browsersync to laravel-mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,8 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
-    .copy('resources/assets/images', 'public/images');
+    .copy('resources/assets/images', 'public/images')
+    .browserSync({
+        proxy: 'localhost:8000'
+    });
+
