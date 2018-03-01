325c65af6626b48c440f1c1fb6a68b92dd312ca8
Manuel Christlieb <manuel.christlieb@googlemail.com>
2018-03-01 18:41:20 +0000

    Apply fixes from StyleCI

--- app/Article.php
+++ app/Article.php
@@ -22,11 +22,11 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model implements Feedable
 {
     protected $guarded = [];
-    
+
     protected $casts = [
         'is_published' => 'boolean',
     ];
-    
+
     /**
      * The attributes that should be mutated to dates.
      *
@@ -35,23 +35,23 @@ class Article extends Model implements Feedable
     protected $dates = [
         'created_at',
         'updated_at',
-        'published_at'
+        'published_at',
     ];
-    
+
     protected static function boot()
     {
         parent::boot();
-        
+
         static::addGlobalScope('published', function ($builder) {
             $builder->where('is_published', true);
         });
     }
-    
+
     public static function getFeedItems()
     {
         return self::all();
     }
-    
+
     public function toFeedItem()
     {
         return FeedItem::create()
@@ -62,17 +62,17 @@ class Article extends Model implements Feedable
             ->link($this->path())
             ->author($this->author->email);
     }
-    
+
     public function path()
     {
         return "/blog/{$this->slug}";
     }
-    
+
     public function image()
     {
         return $this->belongsTo(Image::class);
     }
-    
+
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
@@ -80,7 +80,7 @@ class Article extends Model implements Feedable
     {
         return $this->belongsToMany(Tag::class);
     }
-    
+
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
@@ -88,29 +88,29 @@ class Article extends Model implements Feedable
     {
         return $this->belongsTo(User::class, 'user_id');
     }
-    
+
     public function getRouteKeyName()
     {
         return 'slug';
     }
-    
+
     public function getExcerpt($count = 50)
     {
         preg_match("/(?:\w+(?:\W+|$)){0,$count}/", $this->content, $matches);
-        
-        return $matches[0] . '...';
+
+        return $matches[0].'...';
     }
-    
+
     public function getContentAttribute($body)
     {
         return \Purify::clean($body);
     }
-    
+
     public function readingTime()
     {
         return ceil(str_word_count($this->content) / 250);
     }
-    
+
     /**
      * @param Carbon|null $dateTime
      * @return $this
@@ -118,13 +118,13 @@ class Article extends Model implements Feedable
     public function publish(Carbon $dateTime = null)
     {
         $this->update([
-            'is_published' => !$dateTime ? true : false,
+            'is_published' => ! $dateTime ? true : false,
             'published_at' => $dateTime ?? Carbon::now(),
         ]);
-        
+
         return $this;
     }
-    
+
     /**
      * Get a new query builder that includes archives.
      */
@@ -132,7 +132,7 @@ class Article extends Model implements Feedable
     {
         return (new static)->newQueryWithoutScope('published');
     }
-    
+
     /**
      * Return next post after this one or null.
      *
@@ -143,10 +143,10 @@ class Article extends Model implements Feedable
         $query = static::where('published_at', '>', $this->published_at)
             ->where('published_at', '<=', Carbon::now())
             ->orderBy('published_at', 'asc');
-        
+
         return $query->first();
     }
-    
+
     /**
      * Return older post before this one or null.
      *
@@ -156,7 +156,7 @@ class Article extends Model implements Feedable
     {
         $query = static::where('published_at', '<', $this->published_at)
             ->orderBy('published_at', 'desc');
-        
+
         return $query->first();
     }
 }
