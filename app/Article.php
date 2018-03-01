cfb11effe30fd25d524c04fea345fe46e2837529
Manuel Christlieb <manuel.christlieb@googlemail.com>
2018-03-01 17:21:44 +0000

    Apply fixes from StyleCI

--- app/Article.php
+++ app/Article.php
@@ -3,9 +3,9 @@
 namespace App;
 
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Model;
 use Spatie\Feed\Feedable;
 use Spatie\Feed\FeedItem;
+use Illuminate\Database\Eloquent\Model;
 
 /**
  * @property mixed $author
@@ -21,25 +21,25 @@ use Spatie\Feed\FeedItem;
 class Article extends Model implements Feedable
 {
     protected $guarded = [];
-    
+
     protected $casts = [
         'is_published' => 'boolean',
     ];
-    
+
     protected static function boot()
     {
         parent::boot();
-        
+
         static::addGlobalScope('published', function ($builder) {
             $builder->where('is_published', true);
         });
     }
-    
+
     public static function getFeedItems()
     {
-        return Article::all();
+        return self::all();
     }
-    
+
     public function toFeedItem()
     {
         return FeedItem::create()
@@ -50,17 +50,17 @@ class Article extends Model implements Feedable
             ->link($this->path())
             ->author($this->author->email);
     }
-    
+
     public function path()
     {
         return "/blog/{$this->slug}";
     }
-    
+
     public function image()
     {
         return $this->belongsTo(Image::class);
     }
-    
+
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
@@ -68,7 +68,7 @@ class Article extends Model implements Feedable
     {
         return $this->belongsToMany(Tag::class);
     }
-    
+
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
@@ -76,29 +76,29 @@ class Article extends Model implements Feedable
     {
         return $this->belongsTo(User::class, 'user_id');
     }
-    
+
     public function getRouteKeyName()
     {
         return 'slug';
     }
-    
+
     public function getExcerpt($count = 50)
     {
         preg_match("/(?:\w+(?:\W+|$)){0,$count}/", $this->content, $matches);
-        
-        return $matches[0] . '...';
+
+        return $matches[0].'...';
     }
-    
+
     public function getContentAttribute($body)
     {
         return \Purify::clean($body);
     }
-    
+
     public function readingTime()
     {
         return ceil(str_word_count($this->content) / 250);
     }
-    
+
     /**
      * @param Carbon|null $dateTime
      * @return $this
@@ -106,13 +106,13 @@ class Article extends Model implements Feedable
     public function publish(Carbon $dateTime = null)
     {
         $this->update([
-            'is_published' => !$dateTime ? true : false,
+            'is_published' => ! $dateTime ? true : false,
             'published_at' => $dateTime ?? Carbon::now(),
         ]);
-        
+
         return $this;
     }
-    
+
     /**
      * Get a new query builder that includes archives.
      */
