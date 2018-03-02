31865251489d812b8cb3e0df27303325eb2b7e50
Manuel Christlieb <manuel.christlieb@googlemail.com>
2018-03-02 17:47:27 +0000

    Apply fixes from StyleCI

--- tests/Feature/Admin/UpdateArticlesTest.php
+++ tests/Feature/Admin/UpdateArticlesTest.php
@@ -9,7 +9,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class UpdateArticlesTest extends TestCase
 {
     use RefreshDatabase;
-    
 
     /** @test */
     public function a_user_can_update_articles()
