a421ec63003f8d268eeaf8af6865b1397e5ac662
Manuel Christlieb <manuel.christlieb@googlemail.com>
2018-03-09 15:22:41 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Article;
 use App\Tag;
+use App\Article;
 
 /**
  * Class IndexController.
@@ -18,9 +18,9 @@ class IndexController extends Controller
     public function __invoke()
     {
         $tags = Tag::withCount('articles')->with('image')->orderBy('articles_count', 'desc')->limit(2)->get();
-        
+
         $promotedArticles = Article::whereNotNull('promoted')->with('author', 'tags', 'image')->get();
-        
+
         return view('index', compact('promotedArticles', 'tags'));
     }
 }
