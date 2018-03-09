f59e0e8d52e652806ddde410b399a8733d269d5a
Manuel Christlieb <manuel@christlieb.eu>
2018-03-09 17:18:32 +0100

    change font load implementation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,23 @@
 
     <title>{{ config('app.name', 'christlieb.eu') . ' | ' }}@yield('title', 'Home')</title>
 
+    <script>
+        WebFontConfig = {
+            google: {
+                families: ['Roboto', 'Roboto Condensed', 'Roboto Slab']
+            },
+            timeout: 2000 // Set the timeout to two seconds
+        };
+        (function(d) {
+            let wf = d.createElement('script'), s = d.scripts[0];
+            wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+            wf.async = true;
+            s.parentNode.insertBefore(wf, s);
+        })(document);
+    </script>
     <!-- Styles -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700|Roboto+Slab:400,700|Roboto:400,700" rel="stylesheet" lazyload>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" lazyload>
+    {{--<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700|Roboto+Slab:400,700|Roboto:400,700" rel="stylesheet" lazyload>--}}
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="font-sans">
 <div id="app">
